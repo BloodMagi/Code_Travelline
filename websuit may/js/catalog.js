f65bb6c3576446a5fd5bcecb7960da7688a7ac31
BloodMagi <69989644+BloodMagi@users.noreply.github.com>
2024-05-20 22:05:43 +0300

    Add files via upload

---
+++ websuit may/js/catalog.js
@@ -0,0 +1,10 @@
+<script>
+        function toggleSubcategory(id) {
+            var subcategory = document.getElementById(id);
+            if (subcategory.style.display === "none") {
+                subcategory.style.display = "block";
+            } else {
+                subcategory.style.display = "none";
+            }
+        }
+    </script>
